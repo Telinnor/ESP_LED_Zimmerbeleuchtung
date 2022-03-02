708ce924df2f269dbc7140c2db292c5ccc08c23b
Lars Laptop <sral00@outlook.de>
2022-03-02 14:56:07 +0100

    adding Client_Umsetzung1

---
+++ Umsetzung 1/Client/WIFIsettings_example.cpp
@@ -0,0 +1,13 @@
+
+//PRIVATE
+#ifndef HOMEssid
+#define HOMEssid "HomeWIFIname"
+#define HOMEpsk "0123456789"
+#endif
+
+
+// ESP Access Point
+#ifndef APssid
+#define APssid "ESPap"
+#define APpsk "thereisnospoon"
+#endif
